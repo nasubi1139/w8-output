6d573a7e73e615e432b53b0c1a1af1ccb5a90099
nasubi <smiley.jinro2525@gmail.com>
2025-11-24 23:18:48 +0900

    feature-login js

---
+++ datebase/datebase.js
@@ -0,0 +1,43 @@
+if(('localStorage' in window) && (window.localStorage !== null)) {
+} else {
+}
+
+
+// ボタンを押したときにログイン情報をローカルストレージに保存
+const $click = document.getElementById("js-button");
+const $Email = document.getElementById("Email");
+const $password = document.getElementById("password")
+
+
+$click.addEventListener("click", ()=> {
+  localStorage.setItem("Email",$Email.value);
+  localStorage.setItem("password",$password.value);
+
+
+  if(localStorage.getItem("Email") === "") {
+    localStorage.removeItem("Email");
+  }else {
+  };
+  if(localStorage.getItem("password") === "") {
+    localStorage.removeItem("password");
+  }else {
+  };
+  if((localStorage.getItem("Email") !== null) && (localStorage.getItem("password") !== null)){
+    alert("ログイン成功！")
+  }else {
+    alert("ログイン情報が間違っています")
+  };
+});
+
+console.log(localStorage.getItem("Email"));
+console.log(localStorage.getItem("password"));
+
+// ローカルストレージの情報をHTML要素に挿入
+const loginEmail = localStorage.getItem("Email");
+const loginPass = localStorage.getItem("password");
+
+if((loginEmail !== null) && (loginPass !== null)) {
+  // console.log("Hello")
+  $Email.value = localStorage.getItem("Email");
+  $password.value = localStorage.getItem("password");
+};
